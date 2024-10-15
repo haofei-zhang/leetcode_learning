5a5ae27aefe92ff13d8f121a468f5737a5a10289
haofei-zhang <444835695@qq.com>
2024-10-15 23:15:52 +0800

    lcp68

---
+++ src/main/java/leetcode/problemsLCP68/Main.scala
@@ -0,0 +1,15 @@
+package leetcode.problemsLCP68
+
+/**
+ *
+ * @author Z
+ * @since 2024/10/8 下午11:47
+ */
+object Main {
+
+  def main(args: Array[String]): Unit = {
+
+    println(Solution.beautifulBouquet(Array(1, 2, 3, 2), 1))
+  }
+
+}
